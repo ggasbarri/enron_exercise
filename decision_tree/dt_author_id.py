91a47e8c01c5836fddd757e05d526e1cd3826a03
Gianfranco Gasbarri <gifrgas@gmail.com>
2019-01-03 18:53:52 -0400

    Finished Decision Tree exercise

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -25,6 +25,26 @@ features_train, features_test, labels_train, labels_test = preprocess()
 #########################################################
 ### your code goes here ###
 
+from sklearn import tree
+from sklearn.metrics import accuracy_score as accuracy
+
+clf = tree.DecisionTreeClassifier(min_samples_split=40)
+
+print features_train.shape
+
+t0 = time()  # Calculate training time
+clf.fit(features_train, labels_train)
+print "Training time:", round(time()-t0, 3), "s"
+
+t1 = time()  # Calculate predicting time
+y_pred = clf.predict(features_test)
+print "Prediction time:", round(time()-t1, 3), "s"
+
+t2 = time()  # Calculate accuracy time
+acc = accuracy(labels_test, y_pred)
+print "Accuracy time:", round(time()-t2, 3), "s"
+
+print "Accuracy: %s" % acc
 
 #########################################################
 
